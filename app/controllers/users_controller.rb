9949b9d0eedad80f8b79dc21a962d753ef572efd
Redvan <redvanisation@gmail.com>
2019-09-05 10:33:04 -0400

    starting form_for method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
     end 
     
     def create 
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+      @user = User.new
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
         # @user = User.new(user_params)
 
