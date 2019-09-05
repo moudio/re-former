cc86d5f6c269a018343e91496fc186b8b9abc1b8
Redvan <redvanisation@gmail.com>
2019-09-05 11:57:47 -0400

    adding the update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,21 @@ class UsersController < ApplicationController
     end 
 
     def edit 
-    end 
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      
+
+
+      if @user.update_attributes(user_params)
+        flash[:success] = "User updated!"
+        redirect_to user_path
+      else
+        render action: :edit
+      end
+    end
 
     def show 
     end 
@@ -17,7 +31,7 @@ class UsersController < ApplicationController
          @user = User.new(user_params)
 
         if @user.save
-            redirect_to new_user_path
+            redirect_to new_user_path(@user)
           else
             render :new
           end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates :email, presence: true
-    validates :password, presence: true
+    validates :username, presence: true, length: {maximum: 20}
+    validates :email, presence: true, length: {maximum: 100}
+    validates :password, presence: true, length: {minimum: 6}
 
 end
