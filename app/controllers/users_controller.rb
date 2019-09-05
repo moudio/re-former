174bf93eb93326750c36c232a83d19ff327ce64d
Mouha Diouf <mouhamadoudioufsn@gmail.com>
2019-09-05 15:04:48 +0000

    Adding the edit and show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,18 @@ class UsersController < ApplicationController
         @user = User.new
 
     end 
+
+    def edit 
+    end 
+
+    def show 
+    end 
     
     def create 
 
         # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-        # @user = User.new(user_params)
+         @user = User.new(user_params)
 
         if @user.save
             redirect_to new_user_path
