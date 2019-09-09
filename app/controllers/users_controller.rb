9865158a2b90020776628d7d1d3a6c32f6940cae
Mouha Diouf <mouhamadoudioufsn@gmail.com>
2019-09-09 11:25:24 +0000

    Correcting with rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,42 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def new 
-        @user = User.new
+  def new
+    @user = User.new
+  end
 
-    end 
+  def edit
+    @user = User.find(params[:id])
+  end
 
-    def edit 
-      @user = User.find(params[:id])
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      flash[:success] = 'User updated!'
+      redirect_to user_path
+    else
+      render action: :edit
     end
+  end
 
-    def update
-      @user = User.find(params[:id])
-      
+  def show; end
 
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-      if @user.update_attributes(user_params)
-        flash[:success] = "User updated!"
-        redirect_to user_path
-      else
-        render action: :edit
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path(@user)
+    else
+      render :new
       end
-    end
+  end
 
-    def show 
-    end 
-    
-    def create 
+  private
 
-        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
-         @user = User.new(user_params)
-
-        if @user.save
-            redirect_to new_user_path(@user)
-          else
-            render :new
-          end
-    end 
-
-    private
-
-      def user_params
-        params.require(:user).permit(:username, :email, :password)
-      end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
-class User < ApplicationRecord
-    validates :username, presence: true, length: {maximum: 10}, uniqueness: true
-    validates :email, presence: true, length: {maximum: 100}
-    validates :password, presence: true, length: {minimum: 6}
+# frozen_string_literal: true
 
+class User < ApplicationRecord
+  validates :username, presence: true, length: { maximum: 10 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 100 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
